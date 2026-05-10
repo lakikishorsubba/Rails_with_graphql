aea94e09a88dca00cfa6f101fc693be986ad1d73
lakikishorsubba <lakikishor7@gmail.com>
2026-05-10 13:04:34 +0600

    add integrated connection type in get posts

--- app/graphql/resolvers/post/posts_resolver.rb
+++ app/graphql/resolvers/post/posts_resolver.rb
@@ -1,9 +1,9 @@
 module Resolvers
   module Post
     class PostsResolver < Resolvers::BaseResolver
-      type [ Types::PostType ], null: false
+      type Types::PostType.connection_type, null: false
 
-      def resolve
+      def resolve(**args) # collect all arguement into a ruby hash
         # return  ::Post.all.order(created_at: :asc)
         ::Post.all.order(created_at: :asc)
       end
