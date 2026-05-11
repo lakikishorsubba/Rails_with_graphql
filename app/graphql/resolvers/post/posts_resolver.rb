6108e8bd252b67a65a29a9cffbc8cc24c866cb64
lakikishorsubba <lakikishor7@gmail.com>
2026-05-11 09:58:21 +0600

    add filtering based in author id

--- app/graphql/resolvers/post/posts_resolver.rb
+++ app/graphql/resolvers/post/posts_resolver.rb
@@ -3,13 +3,18 @@ module Resolvers
     class PostsResolver < Resolvers::BaseResolver
       type Types::PostType.connection_type, null: false
       argument :status, Types::PostStatusType, required: false
+      argument :author_id, ID, required: false
 
-      def resolve(status: nil, **args) # collect all arguement into a ruby hash
+      def resolve(status: nil, author_id: nil, **args) # collect all arguement into a ruby hash
         # return  ::Post.all.order(created_at: :asc)
         posts = ::Post.all.order(created_at: :asc)
         # conditonal filtering
         if status.present?
           posts = posts.where(status: status)
+        end
+
+        if author_id.present?
+          posts = posts.where(user_id: author_id)
         end
         posts
       end
