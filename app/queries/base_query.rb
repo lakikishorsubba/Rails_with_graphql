0cbb3ea7092428589e551e53db9fcb4c926db4b9
lakikishorsubba <lakikishor7@gmail.com>
2026-05-11 11:40:10 +0600

    fix filter error

--- app/queries/base_query.rb
+++ app/queries/base_query.rb
@@ -1,7 +1,7 @@
 class BaseQuery # to hold filtering logic
   attr_reader :query, :skip
 
-  def initilaize(params: {}, skip: 0)
+  def initialize(params: {}, skip: 0)
     @params = params
     @skip = skip.to_i
     @query = params[:query]
