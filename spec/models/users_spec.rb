505d440d426debc3328dcbaf007d7d497b25130e
lakikishorsubba <lakikishor7@gmail.com>
2026-05-12 10:39:29 +0600

    add comment

--- spec/models/users_spec.rb
+++ spec/models/users_spec.rb
@@ -1,7 +1,7 @@
-RSpec.describe User, type: :model do
+RSpec.describe(User, type: :model) do # just for redable
   # rspec test cases goes here
   it "user to be valid" do
     user = create(:user)
-    expect(user).to be_valid
+    expect(user).to be_valid # does user.valid?
   end
 end
